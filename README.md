bc5e77d3943d9cd74ed99ab156ab086dc4800e71
Tyler Liu <tyler4long@gmail.com>
2023-11-03 14:03:26 -0700

    Test refresh

--- README.md
+++ README.md
@@ -5,3 +5,9 @@
 These two access tokens have **different** session IDs.
 
 Which means, each token is a different session.
+
+
+## Generate one token and refresh it
+
+Refreshing the token doesn't change the session ID.
+Which means, token refresh will not generate new session. Instead, it will make the existing session live longer.
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import RingCentral from '@rc-ex/core';
 import DebugExtension from '@rc-ex/debug';
+import waitFor from 'wait-for-async';
 
 const rc = new RingCentral({
   server: process.env.RINGCENTRAL_SERVER_URL,
@@ -11,6 +12,9 @@ const main = async () => {
   const debugExtension = new DebugExtension();
   await rc.installExtension(debugExtension);
   await rc.authorize({ jwt: process.env.RINGCENTRAL_JWT_TOKEN! });
-  await rc.authorize({ jwt: process.env.RINGCENTRAL_JWT_TOKEN! });
+  await waitFor({ interval: 3000 });
+  await rc.refresh();
+  await waitFor({ interval: 3000 });
+  await rc.refresh();
 };
 main();
